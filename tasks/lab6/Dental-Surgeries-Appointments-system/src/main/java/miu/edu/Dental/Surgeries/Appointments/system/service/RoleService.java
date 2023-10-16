f42cdb5383523f8e61cbfe2c46a5fa8b77e8b4a4
NaguaG <naranguag@gmail.com>
2023-10-16 16:42:47 -0500

    added roleService

--- tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/service/RoleService.java
+++ tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/service/RoleService.java
@@ -1,2 +1,13 @@
-package miu.edu.Dental.Surgeries.Appointments.system.service;public interface RoleService {
+package miu.edu.Dental.Surgeries.Appointments.system.service;
+
+import miu.edu.Dental.Surgeries.Appointments.system.models.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    List<Role> getAllRoles();
+    Role getRoleById(Long roleId);
+    Role updateRole(Role role);
+    void deleteRole(Role role);
+    Role addNewRole(Role role);
 }
