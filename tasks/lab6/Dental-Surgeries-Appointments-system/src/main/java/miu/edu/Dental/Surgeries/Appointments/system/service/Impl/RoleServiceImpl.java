f4954cf122480dcb3b517a01cac1476d98b2a42d
NaguaG <naranguag@gmail.com>
2023-10-16 16:41:33 -0500

    added roleService

--- tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/service/Impl/RoleServiceImpl.java
+++ tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/service/Impl/RoleServiceImpl.java
@@ -1,2 +1,41 @@
-package miu.edu.Dental.Surgeries.Appointments.system.service.Impl;public class RoleServiceImpl {
+package miu.edu.Dental.Surgeries.Appointments.system.service.Impl;
+
+import miu.edu.Dental.Surgeries.Appointments.system.models.Role;
+import miu.edu.Dental.Surgeries.Appointments.system.repository.RoleRepository;
+import miu.edu.Dental.Surgeries.Appointments.system.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private RoleRepository roleRepository;
+    public RoleServiceImpl(RoleRepository roleRepository){
+        this.roleRepository = roleRepository;
+    }
+    @Override
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Role getRoleById(Long roleId) {
+        return roleRepository.findById(roleId)
+                .orElse(null);
+    }
+
+    @Override
+    public Role updateRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void deleteRole(Role role) {
+        roleRepository.delete(role);
+    }
+
+    @Override
+    public Role addNewRole(Role role) {
+        return roleRepository.save(role);
+    }
 }
