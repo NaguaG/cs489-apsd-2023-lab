7d289c5cb9fad50b868b6105c595ce0ad613f080
NaguaG <naranguag@gmail.com>
2023-10-16 16:43:32 -0500

    added userService

--- tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/service/UserService.java
+++ tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/service/UserService.java
@@ -1,2 +1,13 @@
-package miu.edu.Dental.Surgeries.Appointments.system.service;public interface UserService {
+package miu.edu.Dental.Surgeries.Appointments.system.service;
+
+import miu.edu.Dental.Surgeries.Appointments.system.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> getAllUsers();
+    public User getUserById(Long userId);
+    public User updateUser(User user);
+    public void deleteUser(User user);
+    public User addNewUser(User user);
 }
