1a7dcaf0a0293c9978c952ee4dc14767feb15f31
NaguaG <naranguag@gmail.com>
2023-10-16 16:40:53 -0500

    added userRepo

--- tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/repository/UserRepository.java
+++ tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/repository/UserRepository.java
@@ -2,6 +2,8 @@ package miu.edu.Dental.Surgeries.Appointments.system.repository;
 
 import miu.edu.Dental.Surgeries.Appointments.system.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserService extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
