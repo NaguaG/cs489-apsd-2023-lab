626106ef4e196a9b1512617374208eb103dc1de3
NaguaG <naranguag@gmail.com>
2023-10-17 10:21:27 -0500

    added dummy data

--- tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/DentalSurgeriesAppointmentsSystemApplication.java
+++ tasks/lab6/Dental-Surgeries-Appointments-system/src/main/java/miu/edu/Dental/Surgeries/Appointments/system/DentalSurgeriesAppointmentsSystemApplication.java
@@ -1,9 +1,6 @@
 package miu.edu.Dental.Surgeries.Appointments.system;
 
-import miu.edu.Dental.Surgeries.Appointments.system.models.Appointment;
-import miu.edu.Dental.Surgeries.Appointments.system.models.Dentist;
-import miu.edu.Dental.Surgeries.Appointments.system.models.Patient;
-import miu.edu.Dental.Surgeries.Appointments.system.models.Surgery;
+import miu.edu.Dental.Surgeries.Appointments.system.models.*;
 import miu.edu.Dental.Surgeries.Appointments.system.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @SpringBootApplication
 public class DentalSurgeriesAppointmentsSystemApplication implements CommandLineRunner {
@@ -21,13 +19,17 @@ public class DentalSurgeriesAppointmentsSystemApplication implements CommandLine
 	private final PatientService patientService;
 	private final SurgeryService surgeryService;
 	private final DentistService dentistService;
+	private final UserService userService;
+	private final RoleService roleService;
 
-	public DentalSurgeriesAppointmentsSystemApplication(AddressService addressService, AppointmentService appointmentService, PatientService patientService, SurgeryService surgeryService, DentistService dentistService) {
+	public DentalSurgeriesAppointmentsSystemApplication(AddressService addressService, AppointmentService appointmentService, PatientService patientService, SurgeryService surgeryService, DentistService dentistService, UserService userService, RoleService roleService) {
 		this.addressService = addressService;
 		this.appointmentService = appointmentService;
 		this.patientService = patientService;
 		this.surgeryService = surgeryService;
 		this.dentistService = dentistService;
+		this.userService = userService;
+		this.roleService = roleService;
 
 	}
 
@@ -37,6 +39,11 @@ public class DentalSurgeriesAppointmentsSystemApplication implements CommandLine
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("Adding new users and roles");
+//		var adminRole = addNewRole("admin");
+//		var userRole = addNewRole("user");
+		var adminUser = addNewUser("admin", "pass");
+		var user = addNewUser("user", "pass123");
 		System.out.println("Hello Patients !");
 		var savedPatient1 = addNewPatient("P100", "Gillian", "White");
 		var savedSurgery1 = addNewSurgery("S15");
@@ -70,6 +77,21 @@ public class DentalSurgeriesAppointmentsSystemApplication implements CommandLine
 		var newDentist = new Dentist(lastName, firstName);
 		return dentistService.addNewDentist(newDentist);
 
+	}
+	public Address addNewAddress(String state, String city, String zipcode){
+		var newAddress = new Address(null, state, city, zipcode, null, null);
+		return addressService.addNewAddress(newAddress);
+	}
+	public User addNewUser(String userName, String password){
+		List<Role> roles = List.of(addNewRole("admin"), addNewRole("user"));
+		var newUser = new User(null, userName, password, roles);
+
+		return userService.addNewUser(newUser);
+
+	}
+	public Role addNewRole(String roleName){
+		var newRole = new Role(null, roleName, null);
+		return roleService.addNewRole(newRole);
 	}
 	public Appointment addNewAppointmentAndPatient(LocalDate appointmentDate, LocalTime appointmentTime, Surgery surgery, Patient patient, Dentist dentist){
 		LocalTime time = appointmentTime;
