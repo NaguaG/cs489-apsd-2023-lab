c586d5663839935a66d7a701abbbd8bfc2b8fbb4
NaguaG <naranguag@gmail.com>
2023-10-25 17:49:33 -0500

    lab 9

--- tasks/lab7_restwebapi/Dental_Surgeries_AppSystem_WebApi/src/main/java/miu/edu/Dental_Surgeries_AppSystem_WebApi/model/User.java
+++ tasks/lab7_restwebapi/Dental_Surgeries_AppSystem_WebApi/src/main/java/miu/edu/Dental_Surgeries_AppSystem_WebApi/model/User.java
@@ -1,10 +1,17 @@
 package miu.edu.Dental_Surgeries_AppSystem_WebApi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Singular;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -12,17 +19,95 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+    @Column(nullable = false)
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    @Column(nullable = false)
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "User name is required")
     private String userName;
+    @Column(nullable = false)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8)
     private String password;
-    @ManyToMany
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Email is required")
+    private String email;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "userId")},
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "roleId")}
     )
     private List<Role> roles;
+
+    //Data fields for Userdetails
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private  boolean enabled;
+
+    public User(String userName, String password) {
+        this(null, null, null, userName, password,
+                null, true, true,
+                true, true);
+    }
+
+    public User(Long userId, String firstName, String lastName, String userName, String password,
+                String email, boolean accountNonExpired, boolean accountNonLocked,
+                boolean credentialsNonExpired, boolean enabled) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
+    }
+
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return this.accountNonExpired;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return this.accountNonLocked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return this.accountNonExpired;
+    }
+    @Override
+    public boolean isEnabled(){
+        return this.enabled;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String[] userRoles = getRoles().stream()
+                .map((role) -> role.getRoleName())
+                .toArray(String[]::new);
+        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(userRoles);
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
 }
--- tasks/lab7_restwebapi/Dental_Surgeries_AppSystem_WebApi/src/main/java/miu/edu/Dental_Surgeries_AppSystem_WebApi/repository/RoleRepository.java
+++ tasks/lab7_restwebapi/Dental_Surgeries_AppSystem_WebApi/src/main/java/miu/edu/Dental_Surgeries_AppSystem_WebApi/repository/RoleRepository.java
@@ -4,6 +4,9 @@ import miu.edu.Dental_Surgeries_AppSystem_WebApi.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findRoleByRoleName(String roleName);
 }
